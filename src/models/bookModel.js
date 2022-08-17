48c25df9ac38b49b5911083d3cc421a0dfca5bc1
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2022-08-17 21:20:43 +0530

    assignment_2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        require: true
+    },
+    authorName: String,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    //sales : { type :Number, default : 10},
+    //"best book on earth " ["Nodejs in detail","mongodb in detail","frontend in detail"]
+    // {
+    //     "ch1" : "awesome intro to js",
+    //     "ch2" : " intro to nodejs",
+    //     "ch3" : "intro to db"
+    // }
+    //summary :mongoose.Schema.Types.mixed
+    year: {
+        type: Number,
+        default: "2021"
+    },
+    "totalpages": Number,
+    "stockavailable": Boolean
 }, { timestamps: true });
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,13 +7,37 @@ const BookController= require("../controllers/bookController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
 router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
-
+router.get("/getUsersData/:id", UserController.getUsersData)
+//1
 router.post("/createBook", BookController.createBook  )
+//2
+router.get("/booksData", BookController.booksData)
+//3
+router.post("/getBooksInYear", BookController.getBooksInYear)
+//4
+router.post("/getparticularBOoks", BookController.getparticularBOoks)
+//5
+router.get("/getXINRBooks", BookController.getXINRBooks)
+//6
+router.get("/getRandomBooks", BookController.getRandomBooks)
+//////////////////////////////////////////////////////////////////////////////////////
+//class:- 
 
-router.get("/getBooksData", BookController.getBooksData)
+// router.get("/getBooksData(end point)", BookController.getBooksData)
+
+// router.post("/updateBooks", BookController.updateBooks)
+// router.post("/deleteBooks", BookController.deleteBooks)
+
+// //Moment JS
+// const moment = require ('moment');
+// router.post("/dateManipulations",function(req,res){
+//     const today =moment();
+//     console.log(today.format('YYYY-MM-DD'));
+
+//     let validOrNot
+//     res.send({msg : "all good"})
+// })
 
 module.exports = router;
