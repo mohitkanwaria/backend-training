9c2aa30c6740b7b7dd5acfac68e3f0f511a5957f
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2022-08-18 23:35:46 +0530

    assignment

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,7 +1,7 @@
 const bookModel = require('../models/bookModel')
 const authorModel = require('../models/authorModel')
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //1st
 const createBook = async (req, res) => {
     let data = req.body
@@ -15,23 +15,39 @@ const creatAuthor = async (req, res) => {
     res.send(authorData)
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //2nd
 const byAuthorId = async (req, res) => {
     let data = await authorModel.find({ author_name: "Chetan Bhagat" })
     let book = await bookModel.find({ author_id: { $eq: data[0].author_id } })
-    res.send({ data })
+    res.send({ book })
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//3rd
-// const towStates=async (req, res)=>{
-//     let book=await bookModel.find({name : "Two states"})   
-//     let author=await authorModel.find({author_id :{$eq : data[0].author_id }})
-//     res.send({book})
-// }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-module.exports.createBook = createBook
-module.exports.creatAuthor = creatAuthor
-module.exports.byAuthorId = byAuthorId
-// module.exports.
+//3rd
+
+const towStates=async (req, res)=>{
+    let book=await bookModel.findOneAndUpdate({name : "Two states"}, {$set :{price : 800}}, {new : true});
+    let price=book.price;
+    
+    let authorname= await authorModel.find({author_id:{$eq : book.author_id}}).select({author_name:1, _id :0})
+    res.send({authorname, price});
+} 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//4th
+const bookrange=async (req, res)=>{
+let bookprice=await bookModel.find({price : {$gte : 50, $lte:100}})
+let a= bookprice.map(x=>x.author_id)
+let data= await authorModel.find({author_id : a}).select({author_name:1, _id:0})
+res.send(data)
+
+}
+
+module.exports.createBook=createBook
+module.exports.creatAuthor=creatAuthor
+module.exports.byAuthorId=byAuthorId
+module.exports.towStates=towStates
+module.exports.bookrange=bookrange
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controllers')
 
-router.post('/createBook', controller.createBook)
-
+//1st
+router.post('/createBook', controller.createBook )
 router.post('/createAuthor', controller.creatAuthor)
 
+//2nd
 router.get('/byAuthorId', controller.byAuthorId)
+
+//3rd
+router.get('/updatePrize', controller.towStates)
+
+//4th
+router.get('/bookinfo', controller.bookrange)
 
 module.exports = router;
