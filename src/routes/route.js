41b9f1d97112f92e5f39b801ee9ca11b888212bd
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2022-08-24 22:43:40 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,39 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-// //controller:-we importing the controllers.
-// const authorController = require("../controllers/authorController")
-// const bookController = require("../controllers/bookController")
-// const publishercontrollers = require('../controllers/publisherControllers')
+//controller:-we importing the controllers.
+const authorController = require("../controllers/authorController")
+const bookController = require("../controllers/bookController")
+const publishercontrollers = require('../controllers/publisherControllers')
 
-// // router.get("/test-me", function (req, res) {
-// //     res.send("My first ever api!")
-// // })
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-// //
-// router.post("/createAuthor", authorController.createAuthor)
-// router.post("/createPublisher", publishercontrollers.createPublisher)
-// router.post("/createBook", bookController.createBook)
-// router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
-// router.put("/updateBooks", bookController.updateBooks)
-// router.put("/updatePrice", bookController.updatePrice)
+//
+router.post("/createAuthor", authorController.createAuthor)
+router.post("/createPublisher", publishercontrollers.createPublisher)
+router.post("/createBook", bookController.createBook)
+router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+router.put("/updateBooks", bookController.updateBooks)
+router.put("/updatePrice", bookController.updatePrice)
 
 
 
-// router.get("/getAuthorsData", authorController.getAuthorsData)
-// router.get("/getBooksData", bookController.getBooksData)
+router.get("/getAuthorsData", authorController.getAuthorsData)
+router.get("/getBooksData", bookController.getBooksData)
 
-const  mid1 =function(req,res,next) {
-    console.log("Hi Ia ma a middleware named Midi")
-    //logic
-    let loggedIn = false
-    if (loggedIn == true){
-        next()
-    }
-    else{
-        res.send ("Please login or register")
-    }
-}
-router.get("/basicRoute" ,mid1,UserController.basicCode)
+
 
 module.exports = router;
