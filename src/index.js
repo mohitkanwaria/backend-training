015e4ae12710eb1c01e837d3a4b65be8d8af8b2c
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2022-08-25 15:04:03 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mohitkanwaria_:IZ2vta9qwavx3n0S@cluster0.2byd2qy.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -20,7 +20,26 @@ app.use('/', route);
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        // console.log ("inside GLOBAL MW");
+        let obj =
+        {
+            "name": "mohit",
+            "city": "Dabwali",
+            "address-pincode": "125104" ,
+            // "age" : "23"
+        }
+        // obj.age =12,  //agr obj ke ander value nhi hai to hum usko bahar lekh ke usko obj ke ander ase add karwa sakte hai.
+        // obj["age"] =12,
+
+        console.log(obj.name)
+        console.log(obj["name"])
+        console.log(obj.city)
+        console.log(obj["city"])
+        console.log(obj.address-pincode)
+        console.log(obj["address-pincode"])
+        console.log(obj.age)
+        console.log(obj["age"])
+
         res.send({msg:"done"})
   }
   );
